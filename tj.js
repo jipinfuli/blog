6a8acf7b87a2dcad95148b301300900683473075
jipinfuli <as39328@163.com>
2014-11-23 10:16:47 +0800

    Update tj.js

--- tj.js
+++ tj.js
@@ -1,2 +1,1 @@
-document.writeln("<script language=\"javascript\" type=\"text/javascript\" src=\"http://js.users.51.la/17440402.js\"></script>");
-document.writeln("<noscript><a href=\"http://www.51.la/?17440402\" target=\"_blank\"><img alt=\"&#x6211;&#x8981;&#x5566;&#x514D;&#x8D39;&#x7EDF;&#x8BA1;\" src=\"http://img.users.51.la/17440402.asp\" style=\"border:none\" /></a></noscript>");
+document.writeln("<script language=\"javascript\" src=\"http://count9.51yes.com/click.aspx?id=91473418&logo=12\" charset=\"gb2312\"></script>");

